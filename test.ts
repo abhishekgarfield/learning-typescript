c98e22f1dd4bd02a48f20b57526c6a3526c5ca81
abhishekgarfield <abhishek2759@gmail.com>
2022-11-01 11:10:59 +0530

    learning arrays

--- test.ts
+++ test.ts
@@ -4,6 +4,16 @@ console.log(person) //infer
 
 let u :any =true;
 u="garfield";
-Math.round(u); // type = any
+Math.round(u); // type = any disables type checking 
+
+let arr :string[]=[];
+arr.push("4");  // explicit type array
+
+let arr2=[]; // implicit type array
+arr2.push('hello');
+
+let arr3 :readonly string[]=["helllo"]; // readonly array cannot be mofdified
+
+
 
 
