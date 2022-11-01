f6cc2b9a9a9973cab818b5171555ed833a5ff6d4
abhishekgarfield <abhishek2759@gmail.com>
2022-11-01 10:27:50 +0530

    learning typescript

---
+++ test.ts
@@ -0,0 +1,9 @@
+let person: string ="Abhishek"; // explicit type
+let last_name="sharma" //implicit type
+console.log(person) //infer 
+
+let u :any =true;
+u="garfield";
+Math.round(u); // type = any
+
+
